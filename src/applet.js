1b5dcfd0329bb289fcec42279f67676716acce48
aharo1110 <129776384+aharo1110@users.noreply.github.com>
2025-03-27 13:10:47 -0700

    Checks for schema

--- src/applet.js
+++ src/applet.js
@@ -7,16 +7,18 @@ document.querySelector('#execute-action').addEventListener('click', async () => 
     const selected = document.querySelector("#action-select").value;
     const action = applet.actions[selected];
     let opts = {}
-    Object.keys(action.params_schema.properties).forEach(element => {
-        const paramSchema = action.params_schema.properties[element];
-        const inputValue = document.querySelector(`#${element}-input`).value;
-        if (paramSchema.type === 'number') {
-            opts[element] = parseFloat(inputValue);
-        } else if (paramSchema.type === 'boolean') {
-            opts[element] = inputValue === 'true';
-        } else {
-            opts[element] = inputValue;
-        }
-    });
+    if(action.params_schema){
+        Object.keys(action.params_schema.properties).forEach(element => {
+            const paramSchema = action.params_schema.properties[element];
+            const inputValue = document.querySelector(`#${element}-input`).value;
+            if (paramSchema.type === 'number') {
+                opts[element] = parseFloat(inputValue);
+            } else if (paramSchema.type === 'boolean') {
+                opts[element] = inputValue === 'true';
+            } else {
+                opts[element] = inputValue;
+            }
+        });
+    }
     await applet.sendAction(document.querySelector("#action-select").value, opts);
 });
