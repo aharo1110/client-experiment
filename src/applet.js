24e11b20294aa81ebe0713454979f799f27ad409
Eli <elitkramer@gmail.com>
2025-03-30 14:24:41 -0700

    Run 'prettier'

--- src/applet.js
+++ src/applet.js
@@ -1,24 +1,29 @@
-import { applets } from '@web-applets/sdk';
+import { applets } from "@web-applets/sdk";
 
-const frame = document.querySelector('applet-frame');
+const frame = document.querySelector("applet-frame");
 
-document.querySelector('#execute-action').addEventListener('click', async () => {
+document
+  .querySelector("#execute-action")
+  .addEventListener("click", async () => {
     const applet = await applets.connect(frame.contentWindow);
     const selected = document.querySelector("#action-select").value;
     const action = applet.actions[selected];
-    let opts = {}
-    if(action.params_schema){
-        Object.keys(action.params_schema.properties).forEach(element => {
-            const paramSchema = action.params_schema.properties[element];
-            const inputValue = document.querySelector(`#${element}-input`).value;
-            if (paramSchema.type === 'number') {
-                opts[element] = parseFloat(inputValue);
-            } else if (paramSchema.type === 'boolean') {
-                opts[element] = inputValue === 'true';
-            } else {
-                opts[element] = inputValue;
-            }
-        });
+    let opts = {};
+    if (action.params_schema) {
+      Object.keys(action.params_schema.properties).forEach((element) => {
+        const paramSchema = action.params_schema.properties[element];
+        const inputValue = document.querySelector(`#${element}-input`).value;
+        if (paramSchema.type === "number") {
+          opts[element] = parseFloat(inputValue);
+        } else if (paramSchema.type === "boolean") {
+          opts[element] = inputValue === "true";
+        } else {
+          opts[element] = inputValue;
+        }
+      });
     }
-    await applet.sendAction(document.querySelector("#action-select").value, opts);
-});
+    await applet.sendAction(
+      document.querySelector("#action-select").value,
+      opts
+    );
+  });
--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-const express = require('express');
-const path = require('path');
-const { createServer: createViteServer } = require('vite');
+const express = require("express");
+const path = require("path");
+const { createServer: createViteServer } = require("vite");
 const app = express();
-const APPLET_URL = 'http://localhost:5173'
+const APPLET_URL = "http://localhost:5173";
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 async function setupVite() {
-    const vite = await createViteServer({
-        server: { middlewareMode: true },
-        root: path.join(__dirname, '..')
-    });
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    root: path.join(__dirname, ".."),
+  });
 
-    app.use(vite.middlewares);
+  app.use(vite.middlewares);
 }
 setupVite();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/src', express.static(path.join(__dirname)));
+if (process.env.NODE_ENV === "production") {
+  app.use("/src", express.static(path.join(__dirname)));
 }
 
-app.get('/', async (req, res) => {
-    try {
-        const response = await fetch(`${APPLET_URL}/manifest.json`);
-        const manifest = await response.json();
-        res.render('index', { applet_url: APPLET_URL, manifest });
-    } catch (error) {
-        console.error('Error fetching manifest:', error);
-        res.status(500).send('Error fetching manifest.json');
-    }
-    
+app.get("/", async (req, res) => {
+  try {
+    const response = await fetch(`${APPLET_URL}/manifest.json`);
+    const manifest = await response.json();
+    res.render("index", { applet_url: APPLET_URL, manifest });
+  } catch (error) {
+    console.error("Error fetching manifest:", error);
+    res.status(500).send("Error fetching manifest.json");
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
