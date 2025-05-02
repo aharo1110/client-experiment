6e77f974e50237cc4307b82b830fc00df62e8b4b
Eli <elikramergm@gmail.com>
2025-05-02 15:31:48 -0700

    Run prettier on more stuff

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,20 +1,20 @@
 import { Button } from '@blueprintjs/core';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import 'react-mosaic-component/react-mosaic-component.css';
 import 'react-mosaic-component/styles/index.less';
 import './App.less';
-import { WindowManager, WindowManagerHandle } from './components/WindowManager';
-import { WebviewWindow } from './components/windows/WebviewWindow';
-import { HeadlessWindow } from './components/windows/HeadlessWindow';
-import { useUrls, UrlsProvider } from './contexts/UrlsContext';
 import { UrlSync } from './components/UrlSync';
+import { WindowManager, WindowManagerHandle } from './components/WindowManager';
+import { HeadlessWindow } from './components/windows/HeadlessWindow';
+import { WebviewWindow } from './components/windows/WebviewWindow';
+import { UrlsProvider } from './contexts/UrlsContext';
 
-export const CHAT_URL = "http://localhost:3001";
-export const HOMEPAGE_URL = "http://csumb.edu";
-const NEW_WINDOW_URL = "http://github.com";
+export const CHAT_URL = 'http://localhost:3001';
+export const HOMEPAGE_URL = 'http://csumb.edu';
+const NEW_WINDOW_URL = 'http://github.com';
 
 function App() {
   const windowManager = useRef<WindowManagerHandle>(null);
@@ -51,8 +51,11 @@ function App() {
   return (
     <UrlsProvider>
       <div className="app-header bp5-dark">
-        <Button onClick={onClickAddWindow} text="Add window"
-         style={{fontFamily: "'Segoe UI', system-ui, sans-serif"}} />
+        <Button
+          onClick={onClickAddWindow}
+          text="Add window"
+          style={{ fontFamily: "'Segoe UI', system-ui, sans-serif" }}
+        />
       </div>
       <WindowManager ref={windowManager} />
       <UrlSync />
