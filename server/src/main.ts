ccfd8f694f61193a7f6d9694a04779c396761060
aharo1110 <129776384+aharo1110@users.noreply.github.com>
2025-05-16 09:46:45 -0700

    Applets page

--- server/src/main.ts
+++ server/src/main.ts
@@ -101,3 +101,7 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Chat server running on http://localhost:${PORT}`);
 });
+
+app.get('/applets', (req, res) => {
+  res.render('applets');
+});
